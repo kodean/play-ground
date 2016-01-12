4fd60a7d864d3c172314d62e935e811c15408d5e
kodean <kranthi@productplay.com>
2016-01-11 22:18:49 -0800

    Updated StringDiff

--- algos/StringDiff.java
+++ algos/StringDiff.java
@@ -1,5 +1,6 @@
 /*
- * Print diff between two strings similar to the way the diff utility does between files
+ * Print diff between two strings similar to the way the diff utility does between files:
+ * how to modify first string into second string?
  *
  * diff operation is based on solving longest common subsequence of the two strings.
  * It boils down to two steps.
@@ -15,6 +16,7 @@ class StringDiff {
 
 		System.out.println("Input: \""+str1+"\" and \""+str2+"\", diff:");
 		diff(str1,str2);
+		diff(str2,str1);
 	}
 
 	public static void diff(String str1, String str2) {
@@ -35,28 +37,19 @@ class StringDiff {
 
 		// Step 2
 		int i=1, j=1;
-		while (i<=len1 && j<=len2) {
-			if (str1.charAt(i-1) == str2.charAt(j-1)) {
-				System.out.println(str1.charAt(i-1));
+		while (i<=len1 || j<=len2) {
+			if (i<=len1 && j<=len2 && str1.charAt(i-1) == str2.charAt(j-1)) {
+				System.out.print(str1.charAt(i-1));
 				i++;
 				j++;
 			} else if (j==len2 || matrix[i+1][j] > matrix[i][j+1]) {
-                                System.out.println("-"+str1.charAt(i-1));
+                                System.out.print("(-"+str1.charAt(i-1)+")");
 				i++;
 			} else if (i==len1 || matrix[i+1][j] <= matrix[i][j+1]) {
-                                System.out.println("+"+str2.charAt(j-1));
+                                System.out.print("(+"+str2.charAt(j-1)+")");
 				j++;
 			}
 		}
-		while (i<=len1) {
-			System.out.println("-"+str1.charAt(i-1));
-			i++;
-		}
-		while (j<=len2) {
-			System.out.println("+"+str2.charAt(j-1));
-			j++;
-		}
+		System.out.println();
 	}
 }
-
-
